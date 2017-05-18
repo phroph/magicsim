54bf513002fae28377b49ebb4cfd1bad40a75cad
Phillip Huff <phhuff@microsoft.com>
2017-05-17 23:10:46 -0400

    Trying a better process instrumentation.

--- hadoop/src/main/java/atmasim/hadoop/mapreduce/SimC.java
+++ hadoop/src/main/java/atmasim/hadoop/mapreduce/SimC.java
@@ -12,25 +12,17 @@ public class SimC {
     public static void ExecuteSim(String profilePath) {
         logger.info("Starting SimC process.");
         try {
-            Process simc = new ProcessBuilder("simc", profilePath).start();
+            Process simc = new ProcessBuilder("simc", profilePath).redirectErrorStream(true).start();
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(simc.getInputStream()));
-            BufferedReader eReader = new BufferedReader(new InputStreamReader(simc.getInputStream()));
             StringBuilder builder = new StringBuilder();
-            StringBuilder eBuilder = new StringBuilder();
-            String line = null, eLine = null;
-            while (((line = reader.readLine()) != null) || ((eLine = eReader.readLine()) != null))  {
-                if (line == null) {
-                    // We only get here if reader is finished. So we only read error if no remaining lines. (to prevent blocking)
-                    eBuilder.append(eLine);
-                    eBuilder.append(System.getProperty("line.separator"));
-                } else {
-                    builder.append(line);
-                    builder.append(System.getProperty("line.separator"));
-                }
+            String line = null;
+            while (((line = reader.readLine()) != null))  {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
             }
             logger.info("*** START SIMC LOGS ***");
             logger.info(builder.toString());
-            logger.warn(eBuilder.toString());
             logger.info("*** END SIMC LOGS ***");
             logger.info("Executed sim ("+profilePath+") with status code: " + simc.waitFor());
         } catch (Exception e) {
