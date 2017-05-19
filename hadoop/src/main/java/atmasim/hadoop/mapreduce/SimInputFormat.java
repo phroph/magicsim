ad51ed75b3a423407cef9a8cceaf473bb37e45e4
Phillip Huff <phhuff@microsoft.com>
2017-05-18 20:17:13 -0400

    Cleaning up unnecessary code.
    Should just default to FileInputFormat.

--- hadoop/src/main/java/atmasim/hadoop/mapreduce/SimInputFormat.java
+++ hadoop/src/main/java/atmasim/hadoop/mapreduce/SimInputFormat.java
@@ -2,21 +2,11 @@ package atmasim.hadoop.mapreduce;
 
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.CompressionCodecFactory;
 
 public class SimInputFormat extends FileInputFormat<SimKey, Text> {
-     @Override
-    protected boolean isSplitable(JobContext context, Path file) {
-        CompressionCodec codec = new CompressionCodecFactory(context.getConfiguration()).getCodec(file);
-        return codec == null;
-    }
-
     @Override
     public RecordReader<SimKey, Text> createRecordReader(InputSplit split, TaskAttemptContext context) {
         context.setStatus(split.toString());
