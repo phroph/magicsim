7bfc1e44d0e16d4a1c514c0415aa32a0eb21069a
Phillip Huff <phhuff@microsoft.com>
2018-01-17 18:30:57 -0500

    Nuking everything for a new start.

--- hadoop/converter.js
+++
@@ -1,4 +0,0 @@
-var boss_models = require('../models.js').models;
-var fs = require('fs');
-
-fs.writeFileSync("./src/main/resources/models.json", JSON.stringify(boss_models));
--- node.cmd
+++
@@ -1,4 +0,0 @@
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==32BIT .\nodeexe.exe %*
-if %OS%==64BIT .\nodeexe64.exe %*
-
