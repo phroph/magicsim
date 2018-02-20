8284130581d315c9a35f4894167e003f213dc6eb
Phillip Huff <phhuff@microsoft.com>
2018-02-19 23:51:58 -0500

    Starting the horifying process of Helix scaffolding.
    Created a greedy model for creating an matrix mapping from an abitrary set of points (from the x and y parameters). Added necessary data to properly transform reforge points from gear and map points logically over the space.
    
    Completely untested, almost guaranteed to break spectacularly and produce comically unwanted results, however the basics are there and this gives a decent starting point for proper surface mesh generation from points.

--- magicsim/magicsim/ResultsData.cs
+++ magicsim/magicsim/ResultsData.cs
@@ -13,9 +13,25 @@ using System.Globalization;
 using System.Threading;
 using System.Windows;
 using System.Text.RegularExpressions;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using HelixToolkit.Wpf;
 
 namespace magicsim
 {
+    public enum ColorCoding
+    {
+        /// <summary>
+        /// No color coding, use coloured lights
+        /// </summary>
+        ByLights,
+
+        /// <summary>
+        /// Color code by gradient in y-direction using a gradient brush with white ambient light
+        /// </summary>
+        ByGradientY
+    }
+
     public class ResultsData : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
@@ -106,6 +122,7 @@ namespace magicsim
 
         // Maps player name.
         private Dictionary<string, PlayerReforge> reforges;
+        private Dictionary<string, Player> players;
 
         public ObservableCollection<PlayerReforge> MergedReforges;
 
@@ -153,6 +170,51 @@ namespace magicsim
             }
         }
 
+        public ObservableCollection<Tuple<string,Point3D[,],double[,]>> NamedDataColorValueSets { get; set; }
+
+        public double[,] ColorValues { get; set; }
+
+        public ColorCoding ColorCoding { get; set; }
+
+        public Model3DGroup Lights
+        {
+            get
+            {
+                var group = new Model3DGroup();
+                switch (ColorCoding)
+                {
+                    case ColorCoding.ByGradientY:
+                        group.Children.Add(new AmbientLight(Colors.White));
+                        break;
+                    case ColorCoding.ByLights:
+                        group.Children.Add(new AmbientLight(Colors.Gray));
+                        group.Children.Add(new PointLight(Colors.Red, new Point3D(0, -1000, 0)));
+                        group.Children.Add(new PointLight(Colors.Blue, new Point3D(0, 0, 1000)));
+                        group.Children.Add(new PointLight(Colors.Green, new Point3D(1000, 1000, 0)));
+                        break;
+                }
+                return group;
+            }
+        }
+
+        public Brush SurfaceBrush
+        {
+            get
+            {
+                // Brush = BrushHelper.CreateGradientBrush(Colors.White, Colors.Blue);
+                // Brush = GradientBrushes.RainbowStripes;
+                // Brush = GradientBrushes.BlueWhiteRed;
+                switch (ColorCoding)
+                {
+                    case ColorCoding.ByGradientY:
+                        return BrushHelper.CreateGradientBrush(Colors.Red, Colors.White, Colors.Blue);
+                    case ColorCoding.ByLights:
+                        return Brushes.White;
+                }
+                return null;
+            }
+        }
+
         public ResultsData()
         {
             _results = new List<Tuple<SimResult,string>>();
@@ -170,6 +232,8 @@ namespace magicsim
             playerSpecs = new Dictionary<string, string>();
             playerClasses = new Dictionary<string, string>();
             reforges = new Dictionary<string, PlayerReforge>();
+            players = new Dictionary<string, Player>();
+            NamedDataColorValueSets = new ObservableCollection<Tuple<string, Point3D[,], double[,]>>();
         }
 
         public string GetLabelString()
@@ -214,6 +278,210 @@ namespace magicsim
             });
         }
 
+        public Point3D[,] CreateDataMatrixFromPoints(List<Point3D> points)
+        {
+            List<List<Point3D>> associativeMatrix = new List<List<Point3D>>();
+            List<Point3D> unsearchedSpace = points.OrderBy(x => x.X).ThenBy(x => x.Y).ToList();
+            List<Point3D> unsearchedSubspace = null;
+            Point3D? previous = null;
+            while(unsearchedSpace.Count > 0)
+            {
+                Point3D cursor = new Point3D();
+                if (!previous.HasValue)
+                {
+                    // First of this row. Instantiate a row and pop off the smallest x in the unsearchedSpace and have that parent the new row.
+                    associativeMatrix.Add(new List<Point3D>());
+                    cursor = unsearchedSpace[0];
+                    // The subspace for this row is currently equal to the search space minus the selected cursor, ordered by y ascending to greedily find nearest neighbor.
+                    unsearchedSubspace = unsearchedSpace.Skip(1).OrderBy(x => x.Y).ToList();
+                }
+                else
+                {
+                    if (unsearchedSubspace.Count == 0)
+                    {
+                        // Nothing left in the searchspace. Null out and start a new row.
+                        previous = null;
+                        continue;
+                    }
+                    try
+                    {
+                        // Filter out all the nodes that we believe are in a different row. IE: they are farther to the x-pos direction than they are to the y-pos direction. And take the one with the smallest Y distance to us (closest-in-row).
+                        cursor = unsearchedSubspace.Where(x => x.Y - previous.Value.Y > x.X - previous.Value.X).OrderBy(x => x.Y - previous.Value.Y).First();
+                        // Cut out all nodes that are disencluded from the row (since cursor was the closest we found valid, anything closer than cursor is no longer valid for the row).
+                        // We also skip cursor obviously because it will be added the to searched space at the end of the loop.
+                        // Note: We don't cut out all nodes that were too far away because our row may 'drift' in a direction over time and we don't want to filter out nodes too aggresively that may actually be in our row.
+                        // We can optimize this heuristic in the future.
+                        unsearchedSubspace = unsearchedSubspace.SkipWhile(x => !x.Equals(cursor)).Skip(1).ToList();
+                    }
+                    catch (Exception)
+                    {
+                        // Couldn't find a valid entry in the subspace. Null out so we start a new row.
+                        previous = null;
+                        continue;
+                    }
+                }
+
+                // A node added to the matrix is considered 'searched'.
+                // The matrix is only complete when all nodes are 'searched'
+                unsearchedSpace.Remove(cursor);
+                associativeMatrix.Last().Add(cursor);
+                // To hint where to look for the next node, store reference to the current row tail.
+                previous = cursor;
+            }
+            // Convert associativeMatrix into a multidimensional array. Currently a naive implementation assuming they are front-lined up and get back-padded.
+            var columns = associativeMatrix.Max(x => x.Count);
+            var rows = associativeMatrix.Count;
+            var matrix = new Point3D[rows, columns];
+            for(int i=0; i<rows; i++)
+            {
+                var row = associativeMatrix[i];
+                for(int j=0; j<columns; j++)
+                {
+                    if(j >= row.Count)
+                    {
+                        matrix[i, j] = new Point3D(0.0,0.0,0.0);
+                    } else
+                    {
+                        matrix[i, j] = row[j];
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        public Point3D[,] CreateDataArray(PlayerReforge reforgeData)
+        {
+            // calculate MinX,MaxX, MinY,MaxY
+            // columns*rows should equal total reforges
+            var data = new Point3D[0, 0];
+            var pointList = new List<Point3D>();
+            GearResults gear = players[reforgeData.PlayerName].GetStats();
+            foreach (var point in reforgeData.Dps.Keys)
+            {
+                var stats = 0;
+                bool haste = false, crit = false, mastery = false, vers = false;
+                int total = gear.crit + gear.haste + gear.mastery + gear.vers;
+                double x = 0, y = 0, z = reforgeData.Dps[point];
+                if(point.Crit != 0)
+                {
+                    stats++;;
+                    point.Crit += gear.crit;
+                    crit = true;
+                }
+                if (point.Haste != 0)
+                {
+                    stats++;
+                    point.Haste += gear.haste;
+                    haste = true;
+                }
+                if (point.Mastery != 0)
+                {
+                    stats++;
+                    point.Mastery += gear.mastery;
+                    mastery = true;
+                }
+                if (point.Vers != 0)
+                {
+                    stats++;
+                    point.Vers += gear.vers;
+                    vers = true;
+                }
+                if(stats == 2)
+                {
+                    if (haste)
+                    {
+                        x = (double)point.Haste / (double)total;
+                        if(crit)
+                        {
+                            y = (double)point.Crit / (double)total;
+                        } else if(mastery)
+                        {
+                            y = (double)point.Mastery / (double)total;
+                        } else if(vers)
+                        {
+                            y = (double)point.Vers / (double)total;
+                        }
+                    }
+                    else if (crit)
+                    {
+                        x = (double)point.Crit / (double)total;
+                        if (mastery)
+                        {
+                            y = (double)point.Mastery / (double)total;
+                        }
+                        else if (vers)
+                        {
+                            y = (double)point.Vers / (double)total;
+                        }
+                    }
+                    else if (mastery)
+                    {
+                        x = (double)point.Mastery / (double)total;
+                        y = (double)point.Vers / (double)total;
+                    }
+                }
+                if(stats == 3)
+                {
+                    if(haste)
+                    {
+                        x = (double)point.Haste / (double)total;
+                        if(crit)
+                        {
+                            if(mastery)
+                            {
+                                y = (double)(point.Crit - point.Mastery) / (double)total;
+                            }
+                            else if (vers)
+                            {
+                                y = (double)(point.Crit - point.Vers) / (double)total;
+                            }
+                        } else if(mastery)
+                        {
+                            y = (double)(point.Mastery - point.Vers) / (double)(total);
+                        }
+                    }
+                    else if(crit)
+                    {
+                        x = (double)point.Crit / (double)total;
+                        y = (double)(point.Mastery - point.Vers) / (double)total;
+                    }
+                }
+                if(stats == 4)
+                {
+                    x = (double)(point.Haste - point.Crit) / (double)total;
+                    y = (double)(point.Mastery - point.Vers) / (double)total;
+                }
+                pointList.Add(new Point3D(x, y, z));
+            }
+            return CreateDataMatrixFromPoints(pointList);
+        }
+
+        public double[,] FindGradientY(Point3D[,] data)
+        {
+            int n = data.GetUpperBound(0) + 1;
+            int m = data.GetUpperBound(0) + 1;
+            var K = new double[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    // Finite difference approximation
+                    var p10 = data[i + 1 < n ? i + 1 : i, j - 1 > 0 ? j - 1 : j];
+                    var p00 = data[i - 1 > 0 ? i - 1 : i, j - 1 > 0 ? j - 1 : j];
+                    var p11 = data[i + 1 < n ? i + 1 : i, j + 1 < m ? j + 1 : j];
+                    var p01 = data[i - 1 > 0 ? i - 1 : i, j + 1 < m ? j + 1 : j];
+
+                    //double dx = p01.X - p00.X;
+                    //double dz = p01.Z - p00.Z;
+                    //double Fx = dz / dx;
+
+                    double dy = p10.Y - p00.Y;
+                    double dz = p10.Z - p00.Z;
+
+                    K[i, j] = dz / dy;
+                }
+            return K;
+        }
+
         public void SaveCSVs(string guid)
         {
 
@@ -227,15 +495,16 @@ namespace magicsim
                 var splitIndex = reforge.Item2.IndexOf('_');
                 var time = reforge.Item2.Substring(0, splitIndex);
                 var fight = reforge.Item2.Substring(splitIndex + 1).Split('.')[0];
-                if (model.model.ContainsKey(fight))
+                var timelessFight = reforge.Item2.Split('.')[0];
+                if (model.model.ContainsKey(timelessFight) || model.model.ContainsKey(fight))
                 {
                     if (model.timeModel.Count == 0 || model.timeModel.ContainsKey(time))
                     {
-                        double modelWeight = model.model[fight];
+                        double modelWeight = model.model.ContainsKey(timelessFight) ? model.model[timelessFight] : model.model[fight];
                         double timeWeight = model.timeModel.Count != 0 ? model.timeModel[time] : 1.0;
                         var characterDefinitionRegex = new Regex("([^ ]+) Reforge Plot Results:");
-                        var variableDefinitionRegex = new Regex("([^ ]+, )*([^ ]+)");
-                        var valueRegex = new Regex("([^ ]+, )+");
+                        var variableDefinitionRegex = new Regex("(?:[^ ]+, )*(?:[^ ]+)");
+                        var valueRegex = new Regex("(?:[^, ]+, +)+\\r");
                         var currentName = "";
                         var mapping = new List<string>();
                         PlayerReforge currentReforge = null;
@@ -251,7 +520,7 @@ namespace magicsim
                                 }
                                 currentReforge = reforges[currentName];
                             }
-                            if (valueRegex.IsMatch(reforgeLine))
+                            else if (valueRegex.IsMatch(reforgeLine))
                             {
                                 var values = reforgeLine.Split(',').ToList();
                                 values = values.Take(values.Count() - 1).Select(x => x.Trim()).ToList();
@@ -277,9 +546,17 @@ namespace magicsim
                                     }
                                 }
                                 var dpsError = double.Parse(values.Last());
-                                var dps = double.Parse(values[values.Count - 2], NumberStyles.AllowExponent);
+                                var dps = double.Parse(values[values.Count - 2]);
                                 if (currentReforge != null)
                                 {
+                                    if(!currentReforge.Dps.ContainsKey(point))
+                                    {
+                                        currentReforge.Dps[point] = 0.0;
+                                    }
+                                    if (!currentReforge.DpsError.ContainsKey(point))
+                                    {
+                                        currentReforge.DpsError[point] = 0.0;
+                                    }
                                     currentReforge.Dps[point] += dps * modelWeight * timeWeight;
                                     currentReforge.DpsError[point] += dpsError * modelWeight * timeWeight;
                                 }
@@ -331,11 +608,12 @@ namespace magicsim
                 var splitIndex = result.Item2.IndexOf('_');
                 var time = result.Item2.Substring(0, splitIndex);
                 var fight = result.Item2.Substring(splitIndex + 1).Split('.')[0];
-                if(model.model.ContainsKey(fight))
+                var timelessFight = result.Item2.Split('.')[0];
+                if(model.model.ContainsKey(timelessFight) || model.model.ContainsKey(fight))
                 {
                     if (model.timeModel.Count == 0 || model.timeModel.ContainsKey(time))
                     {
-                        double modelWeight = model.model[fight];
+                        double modelWeight = model.model.ContainsKey(timelessFight) ? model.model[timelessFight] : model.model[fight];
                         double timeWeight = model.timeModel.Count != 0 ? model.timeModel[time]: 1.0;
                         result.Item1.sim.players.ForEach((player) =>
                         {
@@ -343,6 +621,7 @@ namespace magicsim
                             double damage = player.collected_data.dmg.mean;
                             string mainstat = "";
                             double mainstatValue = 0.0;
+                            players[player.name] = player;
                             if (player.scale_factors != null)
                             {
                                 if (player.scale_factors.Int > 0)
--- magicsim/magicsim/SimNode.cs
+++ magicsim/magicsim/SimNode.cs
@@ -10,13 +10,79 @@ namespace magicsim
     {
         public List<Player> players;
     }
-    
+
     public class Player
     {
         public string name;
         public string specialization;
         public ScaleFactors scale_factors;
         public CollectedData collected_data;
+        public Gear gear;
+
+        private void AddGearToResults(GearResults results, GearItem item, bool strength, bool intellect, bool agility)
+        {
+            if (item != null)
+            {
+                if (strength)
+                {
+                    results.strength += item.strength;
+                    results.strength += item.stragiint;
+                }
+                else if (intellect)
+                {
+                    results.intellect += item.intellect;
+                    results.intellect += item.stragiint;
+                }
+                else if (agility)
+                {
+                    results.agility += item.agility;
+                    results.agility += item.stragiint;
+                }
+                results.crit += item.crit_rating;
+                results.haste += item.haste_rating;
+                results.mastery += item.mastery_rating;
+                results.vers += item.versatility_rating;
+            }
+        }
+        public GearResults GetStats()
+        {
+            bool strength = false, intellect = false, agility = false;
+            var results = new GearResults();
+
+            if (collected_data.buffed_stats.attribute.strength > collected_data.buffed_stats.attribute.intellect && collected_data.buffed_stats.attribute.strength > collected_data.buffed_stats.attribute.agility)
+            {
+                strength = true;
+            }
+            if (collected_data.buffed_stats.attribute.agility > collected_data.buffed_stats.attribute.intellect && collected_data.buffed_stats.attribute.agility > collected_data.buffed_stats.attribute.strength)
+            {
+                agility = true;
+            }
+            if (collected_data.buffed_stats.attribute.intellect > collected_data.buffed_stats.attribute.strength && collected_data.buffed_stats.attribute.intellect > collected_data.buffed_stats.attribute.agility)
+            {
+                intellect = true;
+            }
+
+            if (gear != null)
+            {
+                AddGearToResults(results, gear.head, strength, intellect, agility);
+                AddGearToResults(results, gear.neck, strength, intellect, agility);
+                AddGearToResults(results, gear.shoulders, strength, intellect, agility);
+                AddGearToResults(results, gear.chest, strength, intellect, agility);
+                AddGearToResults(results, gear.waist, strength, intellect, agility);
+                AddGearToResults(results, gear.legs, strength, intellect, agility);
+                AddGearToResults(results, gear.feet, strength, intellect, agility);
+                AddGearToResults(results, gear.wrists, strength, intellect, agility);
+                AddGearToResults(results, gear.hands, strength, intellect, agility);
+                AddGearToResults(results, gear.finger1, strength, intellect, agility);
+                AddGearToResults(results, gear.finger2, strength, intellect, agility);
+                AddGearToResults(results, gear.trinket1, strength, intellect, agility);
+                AddGearToResults(results, gear.trinket2, strength, intellect, agility);
+                AddGearToResults(results, gear.back, strength, intellect, agility);
+                AddGearToResults(results, gear.main_hand, strength, intellect, agility);
+                AddGearToResults(results, gear.off_hand, strength, intellect, agility);
+            }
+            return results;
+        }
     }
 
     public class ScaleFactors
@@ -34,6 +100,19 @@ namespace magicsim
     {
         public Dps dps;
         public Damage dmg;
+        public BuffedStats buffed_stats;
+    }
+
+    public class BuffedStats
+    {
+        public Attribute attribute;
+    }
+
+    public class Attribute
+    {
+        public int strength;
+        public int agility;
+        public int intellect;
     }
 
     public class Dps
@@ -44,5 +123,42 @@ namespace magicsim
     public class Damage
     {
         public double mean;
+    }
+
+    public class GearResults
+    {
+        public int strength, intellect, agility, crit, vers, haste, mastery;
+    }
+
+    public class Gear
+    {
+        public GearItem head;
+        public GearItem neck;
+        public GearItem shoulders;
+        public GearItem chest;
+        public GearItem waist;
+        public GearItem legs;
+        public GearItem feet;
+        public GearItem wrists;
+        public GearItem hands;
+        public GearItem finger1;
+        public GearItem finger2;
+        public GearItem trinket1;
+        public GearItem trinket2;
+        public GearItem back;
+        public GearItem main_hand;
+        public GearItem off_hand;
+    }
+
+    public class GearItem
+    {
+        public int crit_rating;
+        public int mastery_rating;
+        public int haste_rating;
+        public int versatility_rating;
+        public int intellect;
+        public int agility;
+        public int strength;
+        public int stragiint;
     }
 }
